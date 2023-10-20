6371fb13f084acb8c03e7859fcd002434498476a
Damien Whitten <dw@chaos404.com>
2023-10-20 11:32:32 -0700

    Import

---
+++ go.mod
@@ -0,0 +1,20 @@
+module github.com/interxfi/flowtest
+
+go 1.21.3
+
+require (
+	github.com/fatih/color v1.15.0
+	github.com/grpc-ecosystem/go-grpc-middleware v1.4.0
+	google.golang.org/grpc v1.59.0
+	google.golang.org/protobuf v1.31.0
+)
+
+require (
+	github.com/golang/protobuf v1.5.3 // indirect
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.17 // indirect
+	golang.org/x/net v0.14.0 // indirect
+	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/text v0.12.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20230822172742-b8732ec3820d // indirect
+)
---
+++ stepper.go
@@ -0,0 +1,270 @@
+package flowtest
+
+import (
+	"encoding/json"
+	"fmt"
+	"reflect"
+	"runtime/debug"
+	"strings"
+
+	"github.com/fatih/color"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"google.golang.org/protobuf/proto"
+)
+
+type Stepper struct {
+	steps    []*step
+	asserter *asserter
+	name     string
+}
+
+func (ss *Stepper) Log(level, message string, fields map[string]interface{}) {
+	if ss.asserter == nil {
+		panic("Log called on stepper without a current step")
+	}
+
+	ss.asserter.logLines = append(ss.asserter.logLines, logLine{
+		level:   level,
+		message: message,
+		fields:  fields,
+	})
+}
+
+func dumpLogLines(t TB, logLines []logLine) {
+	for _, logLine := range logLines {
+		fieldsString := make([]string, 0, len(logLine.fields))
+		for k, v := range logLine.fields {
+			if err, ok := v.(error); ok {
+				v = err.Error()
+			} else if stringer, ok := v.(fmt.Stringer); ok {
+				v = stringer.String()
+			}
+
+			vStr, err := json.MarshalIndent(v, "  ", "  ")
+			if err != nil {
+				vStr = []byte(fmt.Sprintf("ERROR: %s", err))
+			}
+
+			fieldsString = append(fieldsString, fmt.Sprintf("  %s: %s", k, vStr))
+		}
+		levelColor, ok := levelColors[logLine.level]
+		if !ok {
+			levelColor = color.FgRed
+		}
+
+		levelColorPrint := color.New(levelColor).SprintFunc()
+		fmt.Printf("%s: %s\n  %s\n", levelColorPrint(logLine.level), logLine.message, strings.Join(fieldsString, "\n  "))
+	}
+}
+
+var levelColors = map[string]color.Attribute{
+	"DEBUG": color.FgHiWhite,
+	"INFO":  color.FgGreen,
+	"WARN":  color.FgYellow,
+	"ERROR": color.FgRed,
+	"FATAL": color.FgMagenta,
+}
+
+func NewStepper(name string) *Stepper {
+	return &Stepper{
+		name: name,
+	}
+}
+
+type logLine struct {
+	level   string
+	message string
+	fields  map[string]interface{}
+}
+
+type step struct {
+	desc     string
+	asserter *asserter
+	fn       func(t Asserter)
+}
+
+func (ss *Stepper) Step(desc string, fn func(t Asserter)) {
+	ss.steps = append(ss.steps, &step{
+		desc: desc,
+		fn:   fn,
+	})
+}
+
+func (ss *Stepper) RunSteps(t TB) {
+	t.Helper()
+	color.NoColor = false
+	color.New(color.FgCyan).PrintfFunc()("\n>= %s\n", ss.name)
+	blue := color.New(color.FgBlue).PrintfFunc()
+	red := color.New(color.FgRed).PrintfFunc()
+	for idx, step := range ss.steps {
+		asserter := &asserter{
+			t: t,
+		}
+		ss.asserter = asserter
+		step.asserter = asserter
+		blue("STEP %s\n", step.desc)
+		func() {
+			defer func() {
+				if r := recover(); r != nil && r != earlyTestExit {
+
+					asserter.Fatalf("Test Step Panic: %s", r)
+					// Stack will be captured by the Fatal call but too deep
+					asserter.failStack = debug.Stack()
+				}
+			}()
+
+			step.fn(asserter)
+		}()
+		if asserter.failed {
+			for _, previous := range ss.steps[0:idx] {
+				blue("STEP %s - OK\n", previous.desc)
+				dumpLogLines(t, previous.asserter.logLines)
+			}
+
+			red("STEP %s FAILED\n", step.desc)
+			dumpLogLines(t, asserter.logLines)
+			fmt.Printf("Stack: %s\n", string(asserter.failStack))
+			t.FailNow()
+		}
+	}
+}
+
+type TB interface {
+
+	//Cleanup(func())
+	Error(args ...any)
+	Errorf(format string, args ...any)
+	//Fail()
+	FailNow()
+	//Failed() bool
+	Fatal(args ...any)
+	Fatalf(format string, args ...any)
+	Helper()
+	Log(args ...any)
+	Logf(format string, args ...any)
+	//Name() string
+	//Setenv(key, value string)
+	//Skip(args ...any)
+	//SkipNow()
+	//Skipf(format string, args ...any)
+	//Skipped() bool
+	//TempDir() string
+
+}
+type Asserter interface {
+	TB
+
+	NoError(err error)
+	Equal(want, got interface{})
+	CodeError(err error, code codes.Code)
+}
+
+type asserter struct {
+	t         TB
+	logLines  []logLine
+	failed    bool
+	failStack []byte
+}
+
+func (t *asserter) NoError(err error) {
+	t.t.Helper()
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func (t *asserter) Equal(want, got interface{}) {
+	t.t.Helper()
+	if got == nil || want == nil {
+		if got != want {
+			t.t.Fatalf("got %v, want %v", got, want)
+		}
+		return
+	}
+
+	if aProto, ok := got.(proto.Message); ok {
+		bProto, ok := want.(proto.Message)
+		if !ok {
+			t.Fatalf("want was a proto, got was not (%T)", got)
+			return
+		}
+		if !proto.Equal(aProto, bProto) {
+			t.Fatalf("got %v, want %v", got, want)
+		}
+		return
+	}
+
+	if !reflect.DeepEqual(got, want) {
+		t.Fatalf("got %v, want %v", got, want)
+	}
+}
+
+func (t *asserter) CodeError(err error, code codes.Code) {
+	if err == nil {
+		t.t.Fatalf("got no error, want code %s", code)
+		return
+	}
+
+	if s, ok := status.FromError(err); !ok {
+		t.t.Fatalf("got error %s (%T), want code %s", err, err, code)
+	} else {
+		if s.Code() != code {
+			t.t.Fatalf("got code %s, want %s", s.Code(), code)
+		}
+		return
+	}
+}
+
+func (t *asserter) log(level, message string) {
+	t.logLines = append(t.logLines, logLine{
+		level:   level,
+		message: message,
+	})
+}
+
+func (t *asserter) Helper() {
+	t.t.Helper()
+}
+
+func (t *asserter) Log(args ...interface{}) {
+	t.t.Helper()
+	t.log("DEBUG", fmt.Sprint(args...))
+}
+
+func (t *asserter) Logf(format string, args ...interface{}) {
+	t.t.Helper()
+	t.t.Logf(format, args...)
+}
+
+func (t *asserter) Fatal(args ...interface{}) {
+	t.t.Helper()
+	t.log("FATAL", fmt.Sprint(args...))
+	t.FailNow()
+}
+
+func (t *asserter) Fatalf(format string, args ...interface{}) {
+	t.t.Helper()
+	t.log("FATAL", fmt.Sprintf(format, args...))
+	t.FailNow()
+}
+
+func (t *asserter) FailNow() {
+	t.t.Helper()
+	t.failStack = debug.Stack()
+	t.failed = true
+	// TODO: Exit the caller... somehow
+	panic(earlyTestExit)
+}
+
+var earlyTestExit = struct{}{}
+
+func (t *asserter) Error(args ...interface{}) {
+	t.t.Helper()
+	t.t.Error(args...)
+}
+
+func (t *asserter) Errorf(format string, args ...interface{}) {
+	t.t.Helper()
+	t.t.Errorf(format, args...)
+}
